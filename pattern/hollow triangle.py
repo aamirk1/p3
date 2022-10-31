6100e8bfed0e973e9a019a42cb1dd1962f06749b
Mak <mak1312001@gmail.com>
2022-10-31 23:33:12 +0530

    311022 up

---
+++ pattern/hollow triangle.py
@@ -0,0 +1,19 @@
+n =5
+for i in range(1,n+1):
+    for j in range(i):
+        if j==0 or j==i-1:
+            print('*',end=" ")
+        else:
+            if i!=n:
+                print(' ',end=" ")
+            else:
+                print('*',end=" ")
+    print()
+
+    # Output:
+    
+    # * 
+    # * *
+    # *   *
+    # *     *
+    # * * * * *
---
+++ pattern/left triangle.py
@@ -0,0 +1,13 @@
+left = 5
+for i in range(1,left+1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+
+    # Output:
+
+    # * 
+    # * *
+    # * * *
+    # * * * *
+    # * * * * * 
